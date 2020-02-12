b2f969c96df40ae0f1b9e4455b2902d1b97887f6
danilmenka <danilmenka99@gmail.com>
2020-02-12 21:35:05 +0700

    firstAdapterForFines

--- app/src/main/java/com/example/pdd/Adapters/ListViewAdapterForUnpaidFine.java
+++ app/src/main/java/com/example/pdd/Adapters/ListViewAdapterForUnpaidFine.java
@@ -1,4 +1,129 @@
 package com.example.pdd.Adapters;
 
-public class ListViewAdapterForUnpaidFine {
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.example.pdd.DBHelp.DBHelperCars;
+import com.example.pdd.Edit_Add_Auto_Activity;
+import com.example.pdd.R;
+import com.example.pdd.Requests.AsyncPattern;
+
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListViewAdapterForUnpaidFine extends BaseAdapter
+{
+    Activity context;
+    String id[];
+    String text[];
+    String postDate[];
+    String postNum[];
+    String suma[];
+    String totalSuma[];
+    String discountDate[];
+
+
+    public ListViewAdapterForUnpaidFine(Activity context, String id[],String text[],String postDate[],String postNum[],String suma[],String totalSuma[],String discountDate[]) {
+        super();
+        this.context = context;
+        this.id = id;
+        this.text = text;
+        this.postDate = postDate;
+        this.postNum = postNum;
+        this.suma = suma;
+        this.totalSuma = totalSuma;
+        this.discountDate = discountDate;
+    }
+
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return text.length;
+    }
+
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
+
+    private class ViewHolder {
+        TextView txtText;
+        TextView txtPostDate;
+        TextView txtPostNum;
+        TextView txtSuma;
+        TextView txtDiscountDate;
+        TextView txtTotalSuma;
+
+        Button paiButton;
+
+    }
+
+    public View getView(final int position, View convertView, ViewGroup parent)
+    {
+        // TODO Auto-generated method stub
+        ViewHolder holder;
+        LayoutInflater inflater =  context.getLayoutInflater();
+
+        if (convertView == null)
+        {
+            convertView = inflater.inflate(R.layout.list_item_unpdaid, null);
+            holder = new ViewHolder();
+            holder.txtText = (TextView) convertView.findViewById(R.id.text_unpaid3);
+            holder.txtPostDate = (TextView) convertView.findViewById(R.id.text_unpaid1);
+            holder.txtPostNum = (TextView) convertView.findViewById(R.id.text_unpaid4);
+            holder.txtSuma = (TextView) convertView.findViewById(R.id.text_unpaid5);
+            holder.txtDiscountDate = (TextView) convertView.findViewById(R.id.text_unpaid7);
+            holder.paiButton = (Button) convertView.findViewById(R.id.text_unpaid6);
+            convertView.setTag(holder);
+        }
+        else
+        {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.txtText.setText(text[position]);
+        holder.txtPostDate.setText(postDate[position]);
+        holder.txtPostNum.setText(postNum[position]);
+        holder.txtSuma.setText(suma[position]);
+        holder.txtDiscountDate.setText(discountDate[position]);
+
+
+        holder.paiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("TAG",id[position]);
+                Intent intent41= new Intent(context, Edit_Add_Auto_Activity.class);
+             /*   intent41.putExtra("name",names[position]);
+                intent41.putExtra("regNumber",regNumbers[position]);
+                intent41.putExtra("svid",svids[position]);
+                intent41.putExtra("id",id[position]);*/
+                context.startActivity(intent41);
+            }
+        });
+
+        return convertView;
+    }
+    
+
 }
--- app/src/main/java/com/example/pdd/ui/home2/Home2Fragment.java
+++ app/src/main/java/com/example/pdd/ui/home2/Home2Fragment.java
@@ -1,38 +1,126 @@
 package com.example.pdd.ui.home2;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.pdd.Adapters.ListViewAdapterForUnpaidFine;
+import com.example.pdd.DBHelp.DBHelperFines;
+import com.example.pdd.DBHelp.DBHelperUnpaidFines;
 import com.example.pdd.PaidFines_Activity;
+import com.example.pdd.PayActivity;
 import com.example.pdd.R;
 import com.example.pdd.SearchOfNumberPost_Activity;
 import com.example.pdd.Unpaid_fines_Activity;
 
 public class Home2Fragment extends Fragment {
+
+    private ListView lview;
+    private ListViewAdapterForUnpaidFine lviewAdapter;
     private HomeViewModel2 homeViewModel2;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel2 = ViewModelProviders.of(this).get(HomeViewModel2.class);
         View root = inflater.inflate(R.layout.fragment_home2, container, false);
-
         setHasOptionsMenu (true);
-
+        Button button=root.findViewById(R.id.button11);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent411= new Intent(getActivity(), PayActivity.class);
+                startActivity(intent411);
+            }
+        });
         return root;
     }
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menuhome2, menu);
         super.onCreateOptionsMenu(menu, inflater);
+
     }
+
+
+
+    @Override
+    public void onResume() {
+        lview = (ListView) getActivity().findViewById(R.id.list_view_unpay);
+        lview.setClickable(false);
+        String id[];
+        String text[];
+        String postDate[];
+        String postNum[];
+        String suma[];
+        String totalSuma[];
+        String discountDate[];
+
+
+        String svids[];
+        try {
+            DBHelperUnpaidFines dbHelper;
+            dbHelper = new DBHelperUnpaidFines(getActivity());
+            SQLiteDatabase database = dbHelper.getWritableDatabase();
+            Cursor cursor = database.query(DBHelperUnpaidFines.TABLE_UNPAID_FINES, null, null, null, null, null, null);
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            long numRows = DatabaseUtils.queryNumEntries(db, DBHelperUnpaidFines.TABLE_UNPAID_FINES);
+            id = new String [(int)numRows];
+            text = new String [(int)numRows];
+            postDate = new String [(int)numRows];
+            postNum = new String [(int)numRows];
+            suma = new String [(int)numRows];
+            totalSuma = new String [(int)numRows];
+            discountDate = new String [(int)numRows];
+
+            if (cursor.moveToFirst()) {
+                int idIndex = cursor.getColumnIndex(DBHelperUnpaidFines.KEY_IDFINE);
+                int textIndex = cursor.getColumnIndex(DBHelperUnpaidFines.KEY_KOAPTEXT);
+                int postDateIndex = cursor.getColumnIndex(DBHelperFines.KEY_POSTDATE);
+                int postNumIndex = cursor.getColumnIndex(DBHelperFines.KEY_POSTNUM);
+                int sumaIndex = cursor.getColumnIndex(DBHelperUnpaidFines.KEY_SUMA);
+                int totalSumaIndex = cursor.getColumnIndex(DBHelperFines.KEY_TOTALSUMA);
+                int discountDateIndex = cursor.getColumnIndex(DBHelperUnpaidFines.KEY_DISCOUNTDATE);
+
+                int k =0;
+                do {
+                    id[k]=cursor.getString(idIndex);
+                    text[k]=cursor.getString(textIndex);
+                    postDate[k]=cursor.getString(postDateIndex);
+                    postNum[k]=cursor.getString(postNumIndex);
+                    suma[k]=cursor.getString(sumaIndex);
+                    totalSuma[k]=cursor.getString(totalSumaIndex);
+                    discountDate[k]=cursor.getString(discountDateIndex);
+                    k++;
+                } while (cursor.moveToNext());
+            } else Log.d("mLog","0 rows");
+            cursor.close();
+
+            lviewAdapter = new ListViewAdapterForUnpaidFine(getActivity(),id,text,postDate,postNum,suma,totalSuma,discountDate);
+        //    lviewAdapter.registrationCallBackUnpaidFine(this);
+            lview.setAdapter(lviewAdapter);
+
+        } catch (Exception e){
+            Log.e("DB","DB not found");
+        }
+        View empty = getActivity().findViewById(R.id.list_view_textunpay_empty);
+        lview.setEmptyView(empty);
+        super.onResume();
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
